b98dc24753ff3e0c83597b80e79859043fc80c5e
NagendraGit-p <nagendradev393@gmail.com>
2024-10-10 17:36:34 +0530

    varibles

---
+++ 06-arrey.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#index starts from 0, size is 3
+FRUITS=("apple" "kiwi" "orange") #Array
+
+echo "Frist fruit is: ${FRUITS[O]}"
+echo "Frist fruit is: ${FRUITS[O]}"
+echo "Frist fruit is: ${FRUITS[O]}"
+
+echo "Frist fruit is: ${FRUITS[@]}"
