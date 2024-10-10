35140868c2e15a90990a735bd0671b2a396dcff8
NagendraGit-p <nagendradev393@gmail.com>
2024-10-10 16:37:46 +0530

    varibles

---
+++ 03-varibles.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+person1=ramesh
+person2=suresh
+
+
+echo "$person1:: Hi $person2, How are you?"
+echo "$person2: Hello $person1. I am fine. How are you doing?"
+echo "$person1:: I am doing good $person2. What's going on?"
+echo "$person2 :: I started learing shell script $person1"
