f677fb78eea1751b290f4a23948b4adc4e05fb00
NagendraGit-p <nagendradev393@gmail.com>
2024-10-11 10:04:31 +0530

    varibles

---
+++ 09-special-vars.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "All varibles passed to the script: $@"
+echo "Number of varibles passed: $#"
+echo "Script name: $0"
+echo "Current working directory: $pwd"
+echo "Home directory of curent user: $HOME
